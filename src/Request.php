d0280787fc86ecdc9296eb74ef348be3de833c66
Robert Böser <code@robertboeser.de>
2023-08-05 21:13:05 +0200

    added Request class

---
+++ src/Request.php
@@ -0,0 +1,49 @@
+<?php
+namespace Robo\RoboHttp;
+
+class Request {
+    protected $base;
+
+    function __construct($base='') {
+        $this->base = $base;
+    }
+
+    protected function req($path, $opts=[]) {
+        if(!$opts) return file_get_contents($this->base.$path);
+        return file_get_contents(
+            $this->base.$path,
+            false,
+            stream_context_create($opts)
+        );
+    }
+
+    protected function rex($path, $method, $headers=[], $body=null) {
+        $head = '';
+        foreach($headers as $k => $v) {
+            $head .= "$k: $v\r\n";
+        }
+
+        $opts = [
+            'method' => $method,
+            'header' => $head,
+            'content' => $body,
+        ];
+        return $this->req($path, ['http' => $opts]);
+    }
+
+    function get($path) {
+        return $this->req($path);
+    }
+
+    function put($path, $headers, $body=null) {
+        return $this->rex($path, 'PUT', $headers, $body);
+    }
+
+    function post($path, $headers, $body=null) {
+        return $this->rex($path, 'POST', $headers, $body);
+    }
+
+    function delete($path, $headers, $body=null) {
+        return $this->rex($path, 'DELETE', $headers, $body);
+    }
+}
